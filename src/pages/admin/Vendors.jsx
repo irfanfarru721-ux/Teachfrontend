7936270e4a2a60ac00a996ebcfb1fecee7d99b7c
irfanfarru721-ux <irfanfarru721@gmail.com>
2025-11-26 21:49:45 +0530

    Update Vendors.jsx

--- src/pages/admin/Vendors.jsx
+++ src/pages/admin/Vendors.jsx
@@ -1,25 +1,19 @@
-import React, { useEffect, useState } from "react";
-import apiClient from "../../api/apiClient.js";
+import { useState, useEffect } from "react";
+import api from "../../api/apiClient.js";
 
 export default function Vendors() {
   const [vendors, setVendors] = useState([]);
 
   useEffect(() => {
-    apiClient.get("/admin/vendors")
-      .then(res => setVendors(res.data))
-      .catch(err => console.error(err));
+    api.get("/api/vendors").then((res) => setVendors(res.data));
   }, []);
 
   return (
-    <div>
-      <h2>Vendors</h2>
-      <ul>
-        {vendors.map(v => (
-          <li key={v._id}>
-            {v.name} - {v.email}
-          </li>
-        ))}
-      </ul>
-    </div>
+    <>
+      <h1>All Vendors</h1>
+      {vendors.map((v) => (
+        <p key={v._id}>{v.name}</p>
+      ))}
+    </>
   );
 }
