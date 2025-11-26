0cc281227038f010c4011103fdae8157c5b43ae1
irfanfarru721-ux <irfanfarru721@gmail.com>
2025-11-26 21:52:58 +0530

    Update SubCategoryList.jsx

--- src/pages/user/SubCategoryList.jsx
+++ src/pages/user/SubCategoryList.jsx
@@ -1,27 +1,23 @@
-import React, { useEffect, useState } from "react";
 import { useParams, Link } from "react-router-dom";
-import apiClient from "../../api/apiClient.js";
+import { useEffect, useState } from "react";
+import api from "../../api/apiClient.js";
 
 export default function SubCategoryList() {
-  const { vendorId, categoryId } = useParams();
-  const [subcategories, setSubcategories] = useState([]);
+  const { catId } = useParams();
+  const [subs, setSubs] = useState([]);
 
   useEffect(() => {
-    apiClient.get(`/subcategories?vendor=${vendorId}&category=${categoryId}`)
-      .then(res => setSubcategories(res.data))
-      .catch(err => console.error(err));
-  }, [vendorId, categoryId]);
+    api.get(`/api/subcategories/category/${catId}`).then((res) => setSubs(res.data));
+  }, []);
 
   return (
-    <div>
-      <h2>Subcategories</h2>
-      <ul>
-        {subcategories.map(sc => (
-          <li key={sc._1d}>
-            <Link to={`/products/${vendorId}/${categoryId}/${sc._id}`}>{sc.name}</Link>
-          </li>
-        ))}
-      </ul>
-    </div>
+    <>
+      <h1>Subcategories</h1>
+      {subs.map((s) => (
+        <p key={s._id}>
+          <Link to={`/products/${s._id}`}>{s.name}</Link>
+        </p>
+      ))}
+    </>
   );
 }
