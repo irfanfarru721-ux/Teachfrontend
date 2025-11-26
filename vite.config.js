06b440f93b6ee70eef2b6a089c64b8c9725a83c3
irfanfarru721-ux <irfanfarru721@gmail.com>
2025-11-26 23:32:50 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "./",        // important for Render static site
-  server: { port: 5173 }
+  base: "./", // important for Render
 });
