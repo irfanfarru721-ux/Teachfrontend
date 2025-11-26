3ac31843004061b4033fe6ad6a01220c4f016b97
irfanfarru721-ux <irfanfarru721@gmail.com>
2025-11-26 16:38:59 +0530

    Create App.jsx

---
+++ App.jsx
@@ -0,0 +1,45 @@
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar.jsx";
+import Home from "./pages/Home.jsx";
+import LoginRegister from "./pages/LoginRegister.jsx";
+import ProductDetails from "./pages/ProductDetails.jsx";
+import CartPage from "./pages/CartPage.jsx";
+import Checkout from "./pages/Checkout.jsx";
+import Orders from "./pages/Orders.jsx";
+import { AuthProvider } from "./context/AuthContext.jsx";
+import ProtectedRoute from "./components/ProtectedRoute.jsx";
+
+// Admin pages
+import AdminLogin from "./pages/admin/Login.jsx";
+import AdminDashboard from "./pages/admin/Dashboard.jsx";
+import AdminProducts from "./pages/admin/Products.jsx";
+import AdminVendors from "./pages/admin/Vendors.jsx";
+import AdminOrders from "./pages/admin/Orders.jsx";
+import AdminProtectedRoute from "./components/admin/ProtectedRoute.jsx";
+
+export default function App() {
+  return (
+    <AuthProvider>
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          {/* User routes */}
+          <Route path="/login" element={<LoginRegister />} />
+          <Route path="/" element={<ProtectedRoute><Home /></ProtectedRoute>} />
+          <Route path="/product/:id" element={<ProtectedRoute><ProductDetails /></ProtectedRoute>} />
+          <Route path="/cart" element={<ProtectedRoute><CartPage /></ProtectedRoute>} />
+          <Route path="/checkout" element={<ProtectedRoute><Checkout /></ProtectedRoute>} />
+          <Route path="/orders" element={<ProtectedRoute><Orders /></ProtectedRoute>} />
+
+          {/* Admin routes */}
+          <Route path="/admin/login" element={<AdminLogin />} />
+          <Route path="/admin/dashboard" element={<AdminProtectedRoute><AdminDashboard /></AdminProtectedRoute>} />
+          <Route path="/admin/products" element={<AdminProtectedRoute><AdminProducts /></AdminProtectedRoute>} />
+          <Route path="/admin/vendors" element={<AdminProtectedRoute><AdminVendors /></AdminProtectedRoute>} />
+          <Route path="/admin/orders" element={<AdminProtectedRoute><AdminOrders /></AdminProtectedRoute>} />
+        </Routes>
+      </BrowserRouter>
+    </AuthProvider>
+  );
+}
