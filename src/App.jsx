e563bb98485addf84bc3e83e39e70cade23aaf44
irfanfarru721-ux <irfanfarru721@gmail.com>
2025-11-26 22:35:44 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,15 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { AuthProvider } from "./context/AuthContext.jsx";
+import Login from "./pages/auth/Login.jsx";
+
 export default function App() {
-  return <h1>Hello Frontend!</h1>;
+  return (
+    <AuthProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </BrowserRouter>
+    </AuthProvider>
+  );
 }
