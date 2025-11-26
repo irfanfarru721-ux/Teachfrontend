2330debbf28d3c34edb5542294cc8d0e74779739
irfanfarru721-ux <irfanfarru721@gmail.com>
2025-11-27 01:50:30 +0530

    Update CategoryPage.jsx

--- src/pages/CategoryPage.jsx
+++ src/pages/CategoryPage.jsx
@@ -1,1 +1,9 @@
+export default function CategoryPage() {
+  return (
+    <div className="max-w-6xl mx-auto px-4 py-6">
+      <h1 className="text-2xl font-bold">Categories</h1>
+      <p>All product categories will be displayed here.</p>
+    </div>
+  );
+}
 
