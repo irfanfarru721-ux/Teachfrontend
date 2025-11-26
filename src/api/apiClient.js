7b4e42c3da5789c81beae9c30ac1fb89bbabb997
irfanfarru721-ux <irfanfarru721@gmail.com>
2025-11-26 21:46:41 +0530

    Update apiClient.js

--- src/api/apiClient.js
+++ src/api/apiClient.js
@@ -1,16 +1,8 @@
 import axios from "axios";
 
-const API_BASE_URL = "https://srudentbackend-1.onrender.com";
-
-const apiClient = axios.create({
-  baseURL: API_BASE_URL,
-  headers: { "Content-Type": "application/json" },
+const api = axios.create({
+  baseURL: "https://srudentbackend-1.onrender.com",
+  withCredentials: true,
 });
 
-apiClient.interceptors.request.use((config) => {
-  const token = localStorage.getItem("token");
-  if (token) config.headers.Authorization = `Bearer ${token}`;
-  return config;
-});
-
-export default apiClient;
+export default api;
