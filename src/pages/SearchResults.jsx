6175830fd5f967c2c803cec82436cdcac6e4f881
irfanfarru721-ux <irfanfarru721@gmail.com>
2025-11-27 01:52:02 +0530

    Update SearchResults.jsx

--- src/pages/SearchResults.jsx
+++ src/pages/SearchResults.jsx
@@ -1,1 +1,9 @@
+export default function SearchResults() {
+  return (
+    <div className="max-w-6xl mx-auto px-4 py-6">
+      <h1 className="text-2xl font-bold">Search Results</h1>
+      <p>Search results will be displayed here.</p>
+    </div>
+  );
+}
 
