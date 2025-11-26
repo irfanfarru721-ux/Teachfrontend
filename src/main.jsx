49062a2f8019338d1e714056c68bdeeea7115eba
irfanfarru721-ux <irfanfarru721@gmail.com>
2025-11-26 22:28:21 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
-import "./styles/index.css";
-import { AuthProvider } from "./context/AuthContext.jsx";
-import { CartProvider } from "./context/CartContext.jsx";
+import "./styles/index.css"; // make sure this file exists
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <AuthProvider>
-      <CartProvider>
-        <App />
-      </CartProvider>
-    </AuthProvider>
+    <App />
   </React.StrictMode>
 );
