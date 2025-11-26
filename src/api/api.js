e1c15f48158b6954712200b26d407675fb982651
irfanfarru721-ux <irfanfarru721@gmail.com>
2025-11-26 13:44:54 +0530

    Update api.js

--- src/api/api.js
+++ src/api/api.js
@@ -1,17 +1,10 @@
-// src/api/api.js
 import axios from "axios";
 
-// --------------------
-// BASE API
-// --------------------
 export const API = axios.create({
-  baseURL: "https://srudentbackend-1.onrender.com/api",
-  // withCredentials: true, // enable if using cookies
+  baseURL: "https://srudentbackend-1.onrender.com/api", // <-- your backend URL
 });
 
-// --------------------
-// AUTH TOKEN HANDLER
-// --------------------
+// Set auth token
 export const setAuthToken = (token) => {
   if (token) {
     API.defaults.headers.common["Authorization"] = `Bearer ${token}`;
@@ -20,45 +13,6 @@ export const setAuthToken = (token) => {
   }
 };
 
-// --------------------
-// CENTRALIZED REQUEST HANDLER
-// --------------------
-const handleRequest = async (request) => {
-  try {
-    const res = await request();
-    return res.data;
-  } catch (err) {
-    const message = err.response?.data?.message || err.message || "Something went wrong";
-    throw new Error(message);
-  }
-};
-
-// --------------------
-// AUTH
-// --------------------
-export const loginUser = (data) => handleRequest(() => API.post("/auth/login", data));
-export const registerUser = (data) => handleRequest(() => API.post("/auth/register", data));
-
-// --------------------
-// PRODUCTS
-// --------------------
-export const getProducts = () => handleRequest(() => API.get("/products"));
-export const getProduct = (id) => handleRequest(() => API.get(`/products/${id}`));
-export const createProduct = (data) => handleRequest(() => API.post("/products", data));
-export const updateProduct = (id, data) => handleRequest(() => API.put(`/products/${id}`, data));
-export const deleteProduct = (id) => handleRequest(() => API.delete(`/products/${id}`));
-
-// --------------------
-// ORDERS
-// --------------------
-export const createOrder = (data) => handleRequest(() => API.post("/orders", data));
-export const getOrders = () => handleRequest(() => API.get("/orders"));
-
-// --------------------
-// VENDORS
-// --------------------
-export const getVendors = () => handleRequest(() => API.get("/vendors"));
-export const getVendor = (id) => handleRequest(() => API.get(`/vendors/${id}`));
-export const createVendor = (data) => handleRequest(() => API.post("/vendors", data));
-export const updateVendor = (id, data) => handleRequest(() => API.put(`/vendors/${id}`, data));
-export const deleteVendor = (id) => handleRequest(() => API.delete(`/vendors/${id}`));
+// Auth
+export const loginUser = (data) => API.post("/auth/login", data);
+export const registerUser = (data) => API.post("/auth/register", data);
