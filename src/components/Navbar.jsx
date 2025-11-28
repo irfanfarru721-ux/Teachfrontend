41cd434d0fb58f0a382de9b7211b5fb2d7f89978
irfanfarru721-ux <irfanfarru721@gmail.com>
2025-11-28 22:29:05 +0530

    Add files via upload

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+export default function Navbar(){
+  return (
+    <nav>
+      <Link to="/">Home</Link> | <Link to="/products">Products</Link> | <Link to="/cart">Cart</Link> | <Link to="/profile">Profile</Link>
+    </nav>
+  );
+}
